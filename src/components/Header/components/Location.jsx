3cd2ffdeb0c2130e3b879f3ebcd95321ba508104
MariamZead <mariam.a.zead@gmail.com>
2025-11-12 12:26:08 +0200

    remove unnecessary comments

--- src/components/Header/components/Location.jsx
+++ src/components/Header/components/Location.jsx
@@ -3,67 +3,6 @@ import { useCoords } from "../../../hooks/useCoords";
 import useGeolocation from "../../../hooks/useGeolocation";
 import Loading from "../../Loading Screen/Loading";
 
-// export default function LocationBtn() {
-//   const { setCoords } = useCoords();
-//   const { getLocation, loading, disabled } = useGeolocation(setCoords);
-
-//   if (loading) return <Loading />;
-
-//   return (
-//     <button
-//       className="current-location-btn"
-//       onClick={getLocation}
-//       disabled={disabled}
-//     >
-//       <span className="m-icon">my_location</span>
-//       <span className="location-text">Current Location</span>
-//     </button>
-//   );
-// }
-// export default function LocationBtn() {
-//   const { setCoords } = useCoords();
-//   const { getLocation, loading } = useGeolocation(setCoords);
-
-//   return (
-//     <button
-//       className="current-location-btn"
-//       onClick={getLocation}
-//       disabled={loading} // disables while loading
-//     >
-//       <span className="m-icon">my_location</span>
-//       <span className="location-text">
-//         {loading ? "Getting location..." : "Current Location"}
-//       </span>
-//     </button>
-//   );
-// }
-
-// export default function LocationBtn() {
-//   const { coords, setCoords } = useCoords();
-//   const [geoCoords, setGeoCoords] = useState(null); // last geolocation
-//   const { getLocation, loading } = useGeolocation((loc) => {
-//     setCoords(loc);
-//     setGeoCoords(loc); // store geolocation
-//   });
-
-//   // Button is disabled if current coords match last geolocation
-//   const disabled =
-//     geoCoords && coords.lat === geoCoords.lat && coords.lon === geoCoords.lon;
-
-//   return (
-//     <button
-//       className="current-location-btn"
-//       onClick={getLocation}
-//       disabled={loading || disabled} // disabled if fetching or already at current location
-//     >
-//       <span className="m-icon">my_location</span>
-//       <span className="location-text">
-//         {loading ? "Getting location..." : "Current Location"}
-//       </span>
-//     </button>
-//   );
-// }
-
 export default function LocationBtn() {
   const { coords, setCoords } = useCoords();
   const [geoCoords, setGeoCoords] = useState(null);
@@ -80,7 +19,7 @@ export default function LocationBtn() {
     <button
       className="current-location-btn"
       onClick={getLocation}
-      disabled={currentLocation}
+      disabled={currentLocation || loading}
     >
       <span className="m-icon">my_location</span>
       <span className="location-text">
